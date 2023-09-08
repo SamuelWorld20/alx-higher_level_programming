86bce121f4e9a0fc95525eedf96d881e2a54f971
SamuelWorld20 <ejalonibuoluwaseyi@gmail.com>
2023-09-08 06:46:12 -0700

    solution to task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,8 +2,8 @@
 letter = 'a'
 alphabet = ""
 while letter <= 'z':
-    if letter == 'q' and letter == 'e':
+    if letter != 'q' and letter != 'e':
         alphabet += letter
-        letter = chr(ord(letter) + 1)
+    letter = chr(ord(letter) + 1)
 
 print("{}".format(alphabet), end='')
