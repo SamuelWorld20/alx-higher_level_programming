c0be477da6b3a41560232a8ab843b9a9aa974761
SamuelWorld20 <ejalonibuoluwaseyi@gmail.com>
2023-09-27 20:56:42 -0700

    solution to task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    try:
+            for i in range(x):
+                if isinstance(my_list[i], int):
+                    print("{}".format(my_list[i]), end="")
+                    count += 1
+    except IndexError:
+           pass
+    print()
+    return count
