d660480227460e761d8f4add00e6b610bea15af1
SamuelWorld20 <ejalonibuoluwaseyi@gmail.com>
2023-10-11 06:58:18 -0700

    changed file to f

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,5 @@ This is a read_file function
 
 def read_file(filename=""):
     """Reads a text file(UTF8) and prints it to stdout"""
-    with open(filename, "r", encoding="utf-8") as file:
-        print(file.read(), end="")
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
