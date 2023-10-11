488b281425c6925eb77226c4649cff9c06adf264
SamuelWorld20 <ejalonibuoluwaseyi@gmail.com>
2023-10-10 18:59:36 -0700

    solution to task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""This module defines a Square class"""
+
+
+class Square:
+    """Square class with a private instance attribute"""
+    def __init__(self, size):
+        """Initializing the data"""
+        self._size = size
