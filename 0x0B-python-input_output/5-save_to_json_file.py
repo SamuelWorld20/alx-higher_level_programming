828dea4793eb1ed1b09251f22885cd5ff13bc8b2
SamuelWorld20 <ejalonibuoluwaseyi@gmail.com>
2023-10-11 21:16:14 -0700

    half solution to tak 5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import json
+
+"""
+this funct writes an Object to a text file
+"""
+
+
+def save_to_json_file(my_obj, filename):
+    """Object to a text file, using a JSON representation"""
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(my_obj, f)
