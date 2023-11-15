bbbc68b574c18431b44444759ed3339695122794
SamuelWorld20 <ejalonibuoluwaseyi@gmail.com>
2023-11-15 15:57:07 -0800

    implemented a full table with rows and records

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,14 @@
+-- This script creates a table second_table in a database
+-- With multiple rows
+-- And it creates records
+CREATE TABLE IF NOT EXISTS second_table (
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
+INSERT INTO second_table (id, name, score)
+VALUES
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
