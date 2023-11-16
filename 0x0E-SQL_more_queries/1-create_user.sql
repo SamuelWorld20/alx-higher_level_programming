ba8d567bbfa03cda9583c0493ee4a63978534d7a
SamuelWorld20 <ejalonibuoluwaseyi@gmail.com>
2023-11-16 06:24:29 -0800

    script create user giving it password

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- This script creates a user user_0d_1 in the server
+-- The user is granted all previledges on the MySQL server
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
