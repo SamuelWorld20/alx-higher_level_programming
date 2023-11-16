0a734957e52de0d5260c5273799492626cd57121
SamuelWorld20 <ejalonibuoluwaseyi@gmail.com>
2023-11-15 17:00:54 -0800

    fixed error in descending order

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
 -- This script lists the number of records with the same score
-SELECT score, COUNT(*) AS number
-FROM second_table
-GROUP BY score;
-ORDER BY score DESC;
+SELECT `score`, COUNT(*) AS `number`
+FROM `second_table`
+GROUP BY `score`
+ORDER BY `score` DESC;
