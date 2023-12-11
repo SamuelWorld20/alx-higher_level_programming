7e54f791ca76cacbb42d5ce0ced4e4a0efd338a5
SamuelWorld20 <ejalonibuoluwaseyi@gmail.com>
2023-12-11 15:19:00 -0800

    fixed semo standard bug

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const { argv } = require('process');
 console.log(argv.slice(2).length);
-if (argv.slice(2).length == 0) {
+if (argv.slice(2).length === 0) {
   console.log('No argument');
-} else if (argv.slice(2).length == 1) {
+} else if (argv.slice(2).length === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
