b9064cdacd9d6d2d35548611766721c44b336181
SamuelWorld20 <ejalonibuoluwaseyi@gmail.com>
2023-12-11 15:31:00 -0800

    removed log out length

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 const { argv } = require('process');
-console.log(argv.slice(2).length);
 if (argv.slice(2).length === 0) {
   console.log('No argument');
 } else if (argv.slice(2).length === 1) {
