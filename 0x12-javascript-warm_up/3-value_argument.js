f7a5d53e72284c09be0f67a2a6da736970243c02
SamuelWorld20 <ejalonibuoluwaseyi@gmail.com>
2023-12-11 16:03:43 -0800

    fix semistandard error

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const { argv } = require('process');
 if (argv.slice(2).length === 0) {
-	console.log('No argument');
+  console.log('No argument');
 } else {
-	console.log(argv[2]);
+  console.log(argv[2]);
 }
