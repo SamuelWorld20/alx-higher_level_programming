49a6d0efd4196d2c2ad6ad5dd8a35c5e680f28c3
SamuelWorld20 <ejalonibuoluwaseyi@gmail.com>
2023-12-11 16:25:51 -0800

    fixed checker bug

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const { argv } = require('process');
-if (argv.slice(2).length === 0) {
+if (argv[2] === undefined) {
   console.log('No argument');
 } else {
   console.log(argv[2]);
