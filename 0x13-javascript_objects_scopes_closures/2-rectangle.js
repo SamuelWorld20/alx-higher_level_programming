704b89bc2275b9f5e18aa14ad9b9526be01a6993
SamuelWorld20 <ejalonibuoluwaseyi@gmail.com>
2023-12-13 18:54:44 -0800

    fixed undefined errors

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,13 +1,9 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0) {
-      this.rectangle = {};
-    } else {
-      this.rectangle = {
-        width: w,
-        height: h
-      };
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
   }
 }
